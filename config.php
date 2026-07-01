1d18c3592410fdaea9b602df01c2c3506f0a3a0c
Fatima <fatima.munir@algo.com>
2026-07-01 13:34:15 +0500

    fix: use verified Gmail sender

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('BREVO_API_KEY', getenv('BREVO_API_KEY'));
 define('SENDER_NAME',   getenv('SENDER_NAME')   ?: 'FlatLab');
-define('SENDER_EMAIL',  getenv('SENDER_EMAIL')  ?: 'contact@flatlab.io');
+define('SENDER_EMAIL',  getenv('SENDER_EMAIL')  ?: 'fatimamunir99@gmail.com');
 define('SITE_URL',      getenv('SITE_URL')      ?: 'https://flatlabs.infinityfreeapp.com');
