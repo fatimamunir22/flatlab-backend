9416a67d70c0f7eabaf48d332f1e91b5a8d1a706
Fatima <fatima.munir@algo.com>
2026-07-06 17:56:51 +0500

    updated sender address

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('BREVO_API_KEY', getenv('BREVO_API_KEY'));
 define('SENDER_NAME',   getenv('SENDER_NAME')   ?: 'FlatLab');
-define('SENDER_EMAIL',  getenv('SENDER_EMAIL')  ?: 'fatimamunir99@gmail.com');
+define('SENDER_EMAIL',  getenv('SENDER_EMAIL')  ?: 'admin@flatlab.io');
 define('SITE_URL',      getenv('SITE_URL')      ?: 'https://flatlabs.infinityfreeapp.com');
 define('BACKEND_URL',   getenv('BACKEND_URL')   ?: 'https://flatlab-backend-production.up.railway.app');
 define('UNSUB_SECRET',  getenv('UNSUB_SECRET')  ?: 'change-this-secret');
